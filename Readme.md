5d10dfb3f6cebcb6a77b556134527e70c9602e09
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2024-01-13 17:23:18 +0530

    final

---
+++ Readme.md
@@ -0,0 +1,62 @@
+# Tune Tracker
+
+The Favourite Songs Django Application is designed to allow users to manage and showcase their favorite songs, including information about the artists who perform them.
+
+
+## Environment Setup and Project Execution
+
+1. Clone the Repository
+2. Create a Virtual Environment (Recommended)
+``` bash
+python -m venv venv
+```
+3. Activate the Virtual Environment
+
+  On Windows:
+
+``` bash
+.\venv\Scripts\activate
+```
+On macOS/Linux:
+``` bash
+source venv/bin/activate
+```
+4. Install Packages
+``` bash
+pip install -r requirements.txt
+```
+5.Navigate to the Project Directory
+``` bash
+cd FavoriteTunes
+```
+
+6.Run Migrations
+``` bash
+python manage.py makemigrations
+python manage.py migrate
+```
+
+6.Run server
+``` bash
+python manage.py runserver
+```
+# Screenshots
+1. Dashboard
+![Dashboard](screenshots\image.png)
+
+2.Artist
+
+
+3. Songs
+
+
+4. Video Player
+
+5. Add Song
+
+6. Add Artist
+
+7. Song Detail
+![Song Detail](screenshots\songdetail.png)
+
+8.Artist Details
--- TuneTracker/views.py
+++ TuneTracker/views.py
@@ -6,7 +6,7 @@ from .forms import ArtistForm,SongForm
 # Create your views here.
 
 def dashboard(request):
-    songs = Song.objects.all()
+    songs = Song.objects.all().order_by('-id')
     artist = Artist.objects.all()
     
     context = {
@@ -16,19 +16,6 @@ def dashboard(request):
     
     return render(request, 'index.html',context)
 
-def song_detail(request, song_id):
-    song = get_object_or_404(Song, pk=song_id)
-    return render(request, 'song_detail.html', {'song': song})
-
-# def trial(request):
-#     songs = Song.objects.order_by('-id')[:6]
-#     artist = Artist.objects.all()
-    
-#     context = {
-#         'songs':songs,
-#         'artist':artist
-#     }
-#     return render(request, 'trial.html',context)
 
 def artist_detail(request, artist_id):
     artist = get_object_or_404(Artist, id=artist_id)
@@ -80,5 +67,12 @@ def add_song(request):
         form = SongForm()
 
     return render(request, 'add_song.html', {'form': form})
+
+
+def song_detail(request, song_id):
+    song = get_object_or_404(Song, pk=song_id)
+    return render(request, 'song_detail.html', {'song': song})
+
+
 
     
